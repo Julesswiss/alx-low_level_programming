9b80ca2b77fc01ef7966f57001d74ee0adf83442
Julesswiss <jerhivwor@gmail.com>
2022-09-09 14:08:27 -0700

    Software rocks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,13 +7,16 @@
  */
 int main(void)
 {
-	int i, e, g, h, op1, op2;
+int i, e, g, h, op1, op2;
 
-	i = e = g = h = 48;
-	while (h < 58)
+i = e = g = h = 48;
+while (h < 58)
+{
+	g = 48;
+	while (g < 58)
 	{
-		g = 48;
-		while (g < 58)
+		e = 48;
+		while (e < 58)
 		{
 			i = 48;
 			while (i < 58)
