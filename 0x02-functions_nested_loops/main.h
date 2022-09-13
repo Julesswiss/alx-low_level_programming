96cdf95808f08a6560acfd522614ed3d11eabced
Julesswiss <jerhivwor@gmail.com>
2022-09-13 15:42:05 -0700

    FMT

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
