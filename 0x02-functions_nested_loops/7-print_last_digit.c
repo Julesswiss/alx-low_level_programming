a25543044baee55c2e106cf82049ae7c85e42503
Julesswiss <jerhivwor@gmail.com>
2022-09-13 13:52:27 -0700

    FMT

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,8 @@ int print_last_digit(int n)
 	l = n % 10;
 	if (l < 0)
 	{
-		_putchar(-l + 0);
-		return (-l);
+	l *= -1;
 	}
-	else
-	{
-		_putchar(l + 0);
-		return (l);
-	}
+	_putchar(l + '0');
+	return (l);
 }
