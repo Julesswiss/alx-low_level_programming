c2339ea609b7710e798ed4e6340021dad1853930
Julesswiss <jerhivwor@gmail.com>
2022-09-15 07:19:27 -0700

    FMT

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers between  0 - 9.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
