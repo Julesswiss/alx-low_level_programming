b5309b62e3d86fcbd563728dcecadd6fdc62b4f6
Julesswiss <jerhivwor@gmail.com>
2022-09-15 05:39:39 -0700

    FMT

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+C low level prgramming language, C- functions and nested loops.
+15th september 2022. ALX software engineering course. 
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+
+ * Return 1 if true, 0 if false
+ * On error, -1 return.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
