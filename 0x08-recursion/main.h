38ddb724c22c144c93d18c74ac9776443927a8d7
Julesswiss <jerhivwor@gmail.com>
2022-09-28 05:03:11 -0700

    FMT

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,14 +1,15 @@
-#include "main.h"
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-/**
- * _strlen_recursion - returns the length of a string.
- * @s: string
- * Return: the length of a string.
- */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
-}
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
