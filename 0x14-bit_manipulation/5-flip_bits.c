cc9970cd79cc21cbb827c02ec0379b0126895655
Julesswiss <jerhivwor@gmail.com>
2022-10-27 07:48:30 -0700

    FMT

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,42 +1,25 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would
- * need to flip to get from one number to another
- * @n: number one.
- * @m: number two.
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
  *
- * Return: number of bits.
+ * Return: number of bits to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int nbits;
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	for (i = 63; i >= 0; i--)
 	{
-		if ((n & 1) != (m & 1))
-			nbits++;
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
 	}
 
-	return (nbits);
+	return (count);
 }
-
-100
-#include "main.h"
-
-/**
- * get_endianness - checks the endianness
- *
- * Return: 0 if big endian, 1 if little endian
- */
-int get_endianness(void)
-{
-	unsigned int x;
-	char *h;
-
-	x = 1;
-	h = (char *) &x;
-
-	return ((int)*h);
-}
-
