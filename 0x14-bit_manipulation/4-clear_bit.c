7312576402fc0fba34a43991e00bed92e8d59356
Julesswiss <jerhivwor@gmail.com>
2022-10-27 06:58:15 -0700

    FMT

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int k;
+
+	if (index > 63)
+		return (-1);
+
+	k = 1 << index;
+
+	if (*n & k)
+		*n ^= k;
+
+	return (1);
+}
+
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would
+ * need to flip to get from one number to another
+ * @n: number one.
+ * @m: number two.
+ *
+ * Return: number of bits.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int nbits;
+
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			nbits++;
+	}
+
+	return (nbits);
+}
+
+100
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x;
+	char *h;
+
+	x = 1;
+	h = (char *) &x;
+
+	return ((int)*h);
+}
+
