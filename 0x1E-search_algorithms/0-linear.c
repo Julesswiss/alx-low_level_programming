bec6f63a3c54acbd2327d45deffe34cfafe74aea
Julesswiss <jerhivwor@gmail.com>
2023-04-14 02:43:24 +0100

    crazy algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * linear_search - searches for a value in array of
+ * integers using the Linear search in algorithm
+ * @array: array to search for the value in
+ * @size: represents size of the array
+ * @value: value to look for
+ * Return: the index of the found value,
+ * or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!array || size == 0)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
